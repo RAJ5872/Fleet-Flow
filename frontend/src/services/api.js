4415f1c6c75a671f965bd3e88d40021fb25947a6
Raj Panchal <panchalraj1902@gmail.com>
2026-02-21 17:26:58 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://fleet-flow-m41x.onrender.com',
     headers: { 'Content-Type': 'application/json' },
 });
 
